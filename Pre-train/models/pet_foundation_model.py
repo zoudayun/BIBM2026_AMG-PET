e26d99f07a83173bd91cc8db2a68234ece7b8e35
zoudayun <1982146882@qq.com>
2026-07-05 00:06:45 +0800

    Update pet_foundation_model.py

--- Pre-train/models/pet_foundation_model.py
+++ Pre-train/models/pet_foundation_model.py
@@ -16,9 +16,6 @@ def safe_normalize(x, dim=1, eps=1e-6):
 
 
 def mask_mse(input, recon, mask, eps=1e-8):
-    """
-    在"非mask区域"算loss: diff算平方误差
-    """
     mask = 1 - mask
     mask = mask.to(input.dtype)
 
@@ -33,21 +30,10 @@ def mask_mse(input, recon, mask, eps=1e-8):
 
 
 def masked_mse_vector(pred, target, valid_mask, eps=1e-8):
-    """
-    SUVR回归：只在有效ROI上计算
-    pred:       [B, R]
-    target:     [B, R]
-    valid_mask: [B, R]  1表示该ROI在patch里，要参与loss；0表示忽略
-    """
     valid_mask = valid_mask.to(pred.dtype)
     diff = (pred - target).pow(2) * valid_mask
     per_sample = diff.sum(dim=1) / valid_mask.sum(dim=1).clamp_min(eps)
     return per_sample.mean()
-
-
-# =========================================================
-# distributed gather for CLIP
-# =========================================================
 
 
 # =========================================================
@@ -178,11 +164,7 @@ class ClipLoss(nn.Module):
 
 
 class MatchingLoss(nn.Module):
-    """
-    对每个ROI的4种状态做显式matching。
-    logits:  [B, R, S]
-    targets: [B, R, S]
-    """
+
     def __init__(self):
         super().__init__()
 
@@ -201,13 +183,6 @@ class MatchingLoss(nn.Module):
         )
         return loss
 
-
-# class SUVRRegressionLoss(nn.Module):
-#     def __init__(self):
-#         super().__init__()
-
-#     def forward(self, pred_suvr, gt_suvr, aal_label):
-#         return masked_mse_vector(pred_suvr, gt_suvr, aal_label)
     
 
 class SUVRClassificationLoss(nn.Module):
@@ -223,12 +198,12 @@ class SUVRClassificationLoss(nn.Module):
         """
         gt_state = gt_state.long()
         valid_mask = aal_label > 0
-        targets = torch.full_like(gt_state, fill_value=self.ignore_index) # 全部设为ignore
-        cls_mask = valid_mask & (gt_state >= 1) & (gt_state <= 3) # valid ROI 且 gt_state in {1,2,3} 的位置参与分类
+        targets = torch.full_like(gt_state, fill_value=self.ignore_index) 
+        cls_mask = valid_mask & (gt_state >= 1) & (gt_state <= 3) 
         targets[cls_mask] = gt_state[cls_mask] - 1
 
-        pred_logits = pred_logits.reshape(-1, pred_logits.shape[-1])   # [B*156, 3]
-        targets = targets.reshape(-1)                                  # [B*156]
+        pred_logits = pred_logits.reshape(-1, pred_logits.shape[-1])   
+        targets = targets.reshape(-1)                                  
 
         return F.cross_entropy(pred_logits, targets, ignore_index=self.ignore_index)
 
@@ -239,7 +214,7 @@ class PETFoundationModel(nn.Module):
     def __init__(
         self,
         # load token_id tensor(after tokenizer)
-        text_pack_path="/data/junyan/LiangJ/PET_Foundation_Model/aal_text_outputs/aal_text_pack.pt",
+        text_pack_path="aal_text_pack.pt",
         num_aal=170,
         text_dim=512,
         proj_dim=512,
@@ -254,7 +229,7 @@ class PETFoundationModel(nn.Module):
         self.num_aal = num_aal
         self.num_states = 4
         self.suvr_num_states = 3
-        self.stable_roi_idx = [94, 95, 96, 97, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109]  
+        self.stable_roi_idx = []  
         self.suvr_roi_idx = [i for i in range(self.num_aal) if i not in self.stable_roi_idx] # suvr classification（except stable roi）
         self.num_suvr_roi = len(self.suvr_roi_idx)   # 156     
         self.text_dim = text_dim
@@ -278,11 +253,6 @@ class PETFoundationModel(nn.Module):
         self.image_proj = nn.Sequential(nn.Linear(512, text_dim),nn.GELU(),nn.Linear(text_dim, text_dim),)
         self.text_proj = nn.Sequential(nn.Linear(text_dim, text_dim),nn.GELU(),nn.Linear(text_dim, text_dim),)
 
-        # # suvr regression head
-        # self.suvr_head = nn.Linear(512, num_aal)
-
-        # suvr classification head
-        # self.suvr_head = nn.Linear(512, num_aal * self.num_states)
         self.suvr_head = nn.Sequential(
             nn.LayerNorm(512),                          # [B, 512]
             nn.Linear(512, 512),                # [B, hidden_dim]
@@ -295,28 +265,6 @@ class PETFoundationModel(nn.Module):
 
             nn.Linear(512 // 2, self.num_suvr_roi * self.suvr_num_states)   # [B, 156*3]
         )
-
-        # -------------------------
-        # build BiomedCLIP model
-        # -------------------------
-        # def load_biomedclip_local(path):
-        #     import os, json, open_clip
-        #     from open_clip.factory import _MODEL_CONFIGS
-
-        #     with open(os.path.join(path, "open_clip_config.json"), "r") as f:
-        #         cfg = json.load(f)
-
-        #     model_name = "biomedclip_local"
-        #     if model_name not in _MODEL_CONFIGS:
-        #         _MODEL_CONFIGS[model_name] = cfg["model_cfg"]
-
-        #     model, _, preprocess = open_clip.create_model_and_transforms(
-        #         model_name=model_name,
-        #         pretrained=os.path.join(path, "open_clip_pytorch_model.bin"),
-        #         **{f"image_{k}": v for k, v in cfg["preprocess_cfg"].items()},
-        #     )
-
-        #     return model, preprocess
         
         self.biomedclip, self.biomedclip_preprocess = open_clip.create_model_from_pretrained("hf-hub:microsoft/BiomedCLIP-PubMedBERT_256-vit_base_patch16_224")
         self.text_encoder = self.biomedclip.text
@@ -325,7 +273,6 @@ class PETFoundationModel(nn.Module):
         for p in self.biomedclip.parameters():
             p.requires_grad = False
        
-        # CLIP用于对齐：projection后的embedding/text encoder直接输出最终embedding/无projection无output
         if hasattr(self.biomedclip, "text_projection") and self.biomedclip.text_projection is not None:
             actual_text_dim = int(self.biomedclip.text_projection.shape[-1])
         elif hasattr(self.text_encoder, "output_dim"):
@@ -341,15 +288,6 @@ class PETFoundationModel(nn.Module):
         self.context_length = int(self.biomedclip.context_length)
         self.max_text_positions = getattr(getattr(self.text_encoder, "config", None),"max_position_embeddings",self.context_length)
 
-        # -------------------------
-        # load tokenized text pack
-        # expected shape: [4, 170, L]
-        # order of states:
-        #   0 stable uptake
-        #   1 low uptake
-        #   2 medium uptake
-        #   3 high uptake
-        # -------------------------
         pack = torch.load(text_pack_path, map_location="cpu")
         if "tokens" not in pack:
             raise KeyError(f"'tokens' not found in text pack: {text_pack_path}")
@@ -377,7 +315,6 @@ class PETFoundationModel(nn.Module):
 
         text_tokens = text_tokens.long()
         self.register_buffer("text_tokens_all", text_tokens, persistent=True)
-        # text_tokens_all: [4, 170, L] token id tensor
 
 
         # losses
@@ -397,36 +334,8 @@ class PETFoundationModel(nn.Module):
         self.biomedclip.eval()
         return self
 
-    # -----------------------------------------------------
-    # helpers
-    # -----------------------------------------------------
-    # def _encode_all_texts(self):
-    #     """
-    #     整个text直接加载
-    #     把固定的 [4, 170, L] tokens 送入可训练 text encoder，
-    #     得到 [4, 170, 512] 的文本特征
-    #     """
-    #     flat_tokens = self.text_tokens_all.reshape(-1, self.text_token_length).long()  # [4*170, L]
-    #     text_features = self.biomedclip.encode_text(flat_tokens)
-
-    #     if isinstance(text_features, (tuple, list)):
-    #         text_features = text_features[0]
-
-    #     if text_features.shape[-1] != self.text_dim:
-    #         raise ValueError(
-    #             f"text feature dim mismatch: expected {self.text_dim}, got {text_features.shape[-1]}"
-    #         )
-
-    #     text_features = text_features.reshape(self.num_states, self.num_aal, self.text_dim)
-    #     text_features = safe_normalize(text_features, dim=-1)
-    #     return text_features
         
     def _encode_all_texts(self):
-        """
-        把固定的 [4, 170, L] tokens 分 batch
-        送入可训练 text encoder，得到 [4, 170, 512] 的文本特征
-        冻结 text encoder，只训练 text projection head
-        """
         self.biomedclip.eval()
         flat_tokens = self.text_tokens_all.reshape(-1, self.text_token_length).long()  # [4*170, L]
         num_texts = flat_tokens.shape[0]
@@ -455,53 +364,27 @@ class PETFoundationModel(nn.Module):
         text_features = safe_normalize(text_features, dim=-1)
         return text_features
     
-    
-    # 控制image-text相似度在softmax中的“温度”，从而影响模型区分正负样本的能力
     def _get_clip_logit_scale(self):
         if hasattr(self.biomedclip, "logit_scale"):
             return self.biomedclip.logit_scale.exp().clamp(max=100.0)
         return torch.tensor(20.0, device=self.text_tokens_all.device)
 
     def _get_global_image_feature(self, image_features_pyramid_reversed):
-        """
-        reversed pyramid:
-            [deepest, ..., shallowest]
-        deepest feature expected shape [B, 512, d, h, w]
-        """
         deepest_feature = image_features_pyramid_reversed[0]
         pooled = self.global_pool(deepest_feature).flatten(1)  # [B, 512]
         return pooled
 
     def _aggregate_patch_text_feature(self, aal_weight, aal_state, text_features_all):
-        """
-        根据 patch 内有哪些ROI + 每个ROI对应哪个吸收状态，
-        从 self.text_features_all[4, 170, 512] 中聚合出 patch-level 正文本特征
-
-        aal_weight: [B, 170]，可以是0/1，也可以是归一化后的soft weight
-        aal_state: [B, 170], 0/1/2/3
-        return:
-            patch_text_feature: [B, 512]
-        """
+        
         bsz, _ = aal_weight.shape
-        # device = aal_label.device
 
         state_idx = aal_state.long().clamp(min=0, max=self.num_states - 1)  # [B, R]
-
-        # text_features_all: [4, 170, 512]
-        # text_bank = self.text_features_all  # [4, 170, 512]
-
-        # 取出每个样本、每个ROI对应state的文本特征
-        # 先转成 [R, 4, 512] 再方便 gather（每个ROI都有4个状态向量）
-        # 拓展batch维：给每个样本准备一份同样的文本库
         text_bank_r = text_features_all.permute(1, 0, 2).contiguous()      # [R, 4, 512]
-        text_bank_r = text_bank_r.unsqueeze(0).expand(bsz, -1, -1, -1)       # [B, R, 4, 512]
-        
-        # 第4维 512 要展开出来，这样每个 feature 位置都用同一个状态索引(state要复制到 512 个特征维上)
-        # selected [B,170,512]:当前 batch 中，每个样本、每个 ROI 已选好状态后的文本向量
+        text_bank_r = text_bank_r.unsqueeze(0).expand(bsz, -1, -1, -1)       # [B, R, 4, 512]        
+
         gather_idx = state_idx.unsqueeze(-1).unsqueeze(-1).expand(-1, -1, 1, self.text_dim)  # [B, R, 1, 512]
         selected = torch.gather(text_bank_r, dim=2, index=gather_idx).squeeze(2)  # [B, R, 512]
 
-        # 只对 patch 内有的ROI做平均
         weight = aal_weight.float().unsqueeze(-1)  # [B, R, 1]
         pooled = (selected * weight).sum(dim=1)  # [B, 512]
 
@@ -509,13 +392,7 @@ class PETFoundationModel(nn.Module):
         return pooled
 
     def _build_matching_targets(self, aal_label, aal_state):
-        """
-        对每个样本、每个ROI，只在“该ROI存在且对应的真实状态”那个位置标1，其它位置全是0，得到一个 [B,170,4] 的one-hot标签
-        构造 matching 的 target:
-        target[b, r, s] = 1 当且仅当
-            aal_label[b, r] == 1 且 aal_state[b, r] == s
-        否则为0
-        """
+
         bsz, num_roi = aal_label.shape
         device = aal_label.device
 
@@ -529,15 +406,6 @@ class PETFoundationModel(nn.Module):
         return targets
 
     def _compute_matching_logits(self, image_embed, text_features_all):
-        """
-        计算每个图像patch与所有ROI-状态文本语义之间的相似度
-        image_embed: [B, 512]
-        text_features_all: [4, 170, 512]
-
-        输出:
-            logits: [B, 170, 4]
-        """
-        # [4, 170, 512] -> [170, 4, 512]
         text_bank = text_features_all.permute(1, 0, 2).contiguous()  # [R, S, D]
         text_bank = safe_normalize(text_bank,dim=-1)
         logits = torch.einsum("bd,rsd->brs", image_embed, text_bank)  # [B, R, S]
@@ -562,17 +430,7 @@ class PETFoundationModel(nn.Module):
         weight_suvr=1.0,
         return_dict=True,
     ):
-        """
-        inputs:
-            image:      [B, 1, H, W, D]
-            mim_mask:   [B, 1, H, W, D]
-            mask_image: [B, 1, H, W, D]
-            aal_label:  [B, 170]
-            aal_patch_ratio: [B, 170]
-            aal_cover_ratio: [B, 170]
-            aal_state:  [B, 170]
-            aal_suvr:   [B, 170]
-        """
+        
         # -------------------------
         # encode image
         # -------------------------
@@ -583,11 +441,6 @@ class PETFoundationModel(nn.Module):
         # deepest pooled feature
         pooled_feat = self._get_global_image_feature(image_features_pyramid)  # [B, 512]
 
-        # projected image embedding for clip / matching
-        # image_embed = self.image_proj(pooled_feat)  # [B, 512]
-
-        # image_embed normalize
-        # image_embed = safe_normalize(pooled_feat, dim=-1)
         image_embed = safe_normalize(self.image_proj(pooled_feat), dim=-1)
 
         need_text_features = (
@@ -624,18 +477,14 @@ class PETFoundationModel(nn.Module):
             aal_label_float = aal_label.float()
             aal_patch_ratio = aal_patch_ratio.float()
             aal_cover_ratio = aal_cover_ratio.float()
-            
-            # 计算patch中ROI的权重
+
             weights_aal = aal_label_float * torch.sqrt(
                 torch.clamp(aal_patch_ratio, min=1e-8)
                 * torch.clamp(aal_cover_ratio, min=1e-8)
             )
-            weights_aal = weights_aal / (weights_aal.sum(dim=1, keepdim=True) + 1e-8) # 归一化变成概率分布（和为1）
+            weights_aal = weights_aal / (weights_aal.sum(dim=1, keepdim=True) + 1e-8)
 
-            # ----------------------
-            # input：weights_aal：[B, 170]、aal_state：[B, 170]（0~3）、text_features_all：[4, 170, 512]
-            # output: patch_text_feature: [B, 512]
-            # ----------------------
+
             patch_text_feature = self._aggregate_patch_text_feature(
                 weights_aal,
                 aal_state,
@@ -661,16 +510,6 @@ class PETFoundationModel(nn.Module):
             matching_logits = None
             matching_targets = None
             matching_loss = torch.tensor(0.0, device=image.device)
-
-        # # -------------------------
-        # # 4) suvr regression
-        # # -------------------------
-        # if (aal_label is not None) and (aal_suvr is not None) and (weight_suvr > 0):
-        #     pred_suvr = self.suvr_head(pooled_feat)  # [B, 170]
-        #     suvr_loss = self.suvr_loss(pred_suvr, aal_suvr, aal_label)
-        # else:
-        #     pred_suvr = None
-        #     suvr_loss = torch.tensor(0.0, device=image.device)
 
         # -------------------------
         # 4) suvr classification
